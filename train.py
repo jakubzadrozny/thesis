aa319becde78d02dbb1ccc2a4e882ad6ef1eff6a
Jakub Zadrożny <>
2019-04-24 00:56:54 +0200

    enabled training on multiple gpus

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+import torch.cuda
+import torch.nn as nn
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 
@@ -5,12 +7,14 @@ from datasets import ModelnetDataset, FAVOURITE_CLASS
 from models import VAE, ENCODER_HIDDEN, DECODER_LAYERS
 
 INF = 1e60
+USE_CUDA = torch.cuda.is_available()
+MULTIPLE_GPUS = torch.cuda.device_count() > 1
 
-def train_model(model, optimizer, loader, mc_samples=1, beta=1.0, lbd=0.0, num_epochs=100, use_cuda=True):
+def train_model(model, optimizer, loader, mc_samples=1, beta=1.0, lbd=0.0, num_epochs=100):
     print('Training your model!\n')
     model.train()
-    if use_cuda:
-        model.to('cuda')
+    if USE_CUDA:
+        model.cuda()
 
     global_step = 0
     best_params = None
@@ -19,7 +23,7 @@ def train_model(model, optimizer, loader, mc_samples=1, beta=1.0, lbd=0.0, num_e
     try:
         for epoch in range(num_epochs):
             for inum, batch in enumerate(loader):
-                if use_cuda:
+                if USE_CUDA:
                     batch = batch.cuda()
 
                 global_step += 1
@@ -48,6 +52,8 @@ if __name__ == '__main__':
     train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=4)
 
     model = VAE(ENCODER_HIDDEN, DECODER_LAYERS)
+    if MULTIPLE_GPUS:
+        model = nn.DataParallel(model)
     optimizer = Adam(model.parameters(), lr=2e-4)
 
-    train_model(model, optimizer, train_loader, lbd=4.0, mc_samples=10, num_epochs=1000, use_cuda=True)
+    train_model(model, optimizer, train_loader, lbd=10.0, mc_samples=10, num_epochs=3000)
